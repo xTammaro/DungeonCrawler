b1d666d19c7a8746043b30be613ee19ce79995a4
Alex Boxall <u7468248@anu.edu.au>
2023-08-30 22:22:11 +1000

    Added sample test case, changed Gradle files to get it to work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
+    public static int testFunction() {
+        return 1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!!");
     }
 }
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,11 @@
+import org.example.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DummyTest {
+    @Test
+    public void test() {
+        assertEquals(Main.testFunction(), 1);
+    }
+}
