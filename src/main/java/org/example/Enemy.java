4710bc48543e11c0ad8c75ac576943975609f96a
xTammaro <jaketammaro@gmail.com>
2023-09-06 14:44:15 +1000

    Random location generator class

--- src/main/java/org/example/Enemy.java
+++ src/main/java/org/example/Enemy.java
@@ -1,24 +1,39 @@
 package org.example;
 
+import java.util.Random;
+
 /**
  * Represents an enemy on the board.
  */
 public class Enemy extends Actor {
+    /**
+     * The target location that the enemy is moving towards.
+     */
     private int targetX;
     private int targetY;
-    private static final int MOVE_DISTANCE = 5;
+
+    /**
+     * Set distance for how far the enemy randomly moves towards.
+     */
+    private static final int LOCATION_DISTANCE = 5;
+
+    /**
+     * Current mode of the enemy.
+     */
     private EnemyMode currentMode;
+
     /**
      * Enemy constructor.
      *
-     * @author Alex Boxall
-     *
-     * @param x The initial X position
-     * @param y The initial Y position
+     * @param x  The initial X position
+     * @param y  The initial Y position
      * @param hp The initial health
+     * @author Alex Boxall
      */
     Enemy(int x, int y, int hp) {
         super(x, y, hp);
+        this.currentMode = EnemyMode.RoamMode;
+        newRandomLocation();
     }
 
     /**
@@ -32,6 +47,52 @@ public class Enemy extends Actor {
     }
 
     /**
+     * Called when the enemy is in RoamMode. Should set the enemy towards the target location.
+     *
+     * @author Jake Tammaro
+     */
+
+    void newRandomLocation() {
+        Random rand = new Random();
+        int direction = rand.nextInt(8);  // 0 to 7 representing 8 possible directions
+
+        switch (direction) {
+            case 0:  // North
+                targetX = getX();
+                targetY = getY() - LOCATION_DISTANCE;
+                break;
+            case 1:  // South
+                targetX = getX();
+                targetY = getY() + LOCATION_DISTANCE;
+                break;
+            case 2:  // East
+                targetX = getX() + LOCATION_DISTANCE;
+                targetY = getY();
+                break;
+            case 3:  // West
+                targetX = getX() - LOCATION_DISTANCE;
+                targetY = getY();
+                break;
+            case 4:  // North-East
+                targetX = getX() + LOCATION_DISTANCE;
+                targetY = getY() - LOCATION_DISTANCE;
+                break;
+            case 5:  // North-West
+                targetX = getX() - LOCATION_DISTANCE;
+                targetY = getY() - LOCATION_DISTANCE;
+                break;
+            case 6:  // South-East
+                targetX = getX() + LOCATION_DISTANCE;
+                targetY = getY() + LOCATION_DISTANCE;
+                break;
+            case 7:  // South-West
+                targetX = getX() - LOCATION_DISTANCE;
+                targetY = getY() + LOCATION_DISTANCE;
+                break;
+        }
+    }
+
+    /**
      * Called when the enemy dies. Should remove the enemy from the GameState.
      *
      * @author Alex Boxall
@@ -39,5 +100,14 @@ public class Enemy extends Actor {
     @Override
     void onZeroHealth() {
         GameState.getInstance().enemies.remove(this);
+    }
+
+
+    private int getY() {
+        return y;
+    }
+
+    private int getX() {
+        return x;
     }
 }
