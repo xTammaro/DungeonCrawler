8baa107214542a1e58d52bd7cfd9683ee98055b2
xTammaro <jaketammaro@gmail.com>
2023-09-08 15:11:33 +1000

    Created random location method.

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -5,4 +5,28 @@ package org.example;
  */
 public class Board {
     Tile[][] tiles;
+    int width;
+    int height;
+
+
+    /**
+     * @author Jake Tammaro
+     * Checks if a given tile is empty.
+     * @param tile
+     * @return true if the tile is empty, false otherwise
+     */
+
+    boolean isEmptyTile(Tile tile) {
+        return tile == Tile.Empty || tile == Tile.EmptyWithKey;
+    }
+
+    boolean isOutOfBounds(int x, int y) {
+        return x < 0 || x >= width || y < 0 || y >= height;
+    }
+
+
+
+    public Tile[][] getTiles() {
+        return tiles;
+    }
 }
