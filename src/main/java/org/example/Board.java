3dd9111e1f92cfa5aa83404095c515304688ee62
Tal Shy-Tielen <u7492883@anu.edu.au>
2023-09-10 19:42:33 +1000

    Added getTile to board class

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -5,4 +5,19 @@ package org.example;
  */
 public class Board {
     Tile[][] tiles;
+
+    public Board(int x, int y) {
+        this.tiles = new Tile[x][y];
+    }
+
+    public Tile getTile(int x, int y) {
+        if (x >= tiles.length || x < 0) {
+            return null;
+        }
+        if (y >= tiles[0].length || y < 0) {
+            return null;
+        }
+        return tiles[x][y];
+    }
+
 }
