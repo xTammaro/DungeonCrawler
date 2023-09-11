09bd845be4f07b1908e6c93d8dd3b9e32574a65e
Alex Boxall <u7468248@anu.edu.au>
2023-09-11 10:39:49 +1000

    Only array indexes (i.e. [y][x]) are in that order, function arguments can remain in x, y order.

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -8,27 +8,27 @@ public class Board {
 
     /**
      * @author Tal Shy-Tielen
-     * @param y height of new board.
      * @param x width of the new board.
+     * @param y height of new board.
      */
-    public Board(int y, int x) {
+    public Board(int x, int y) {
         this.tiles = new Tile[y][x];
     }
     /**
      * @author Tal Shy-Tielen
-     * @param y y coordinate of tile.
      * @param x x coordinate of tile.
+     * @param y y coordinate of tile.
      * @return the tile at that position. If the tile is not on the board it will return null.
      * Note: function assumes the board is at least one tile tall.
      */
-    public Tile getTile(int y, int x) {
+    public Tile getTile(int x, int y) {
         if (x >= tiles[0].length || x < 0) {
             return null;
         }
         if (y >= tiles.length || y < 0) {
             return null;
         }
-        return tiles[x][y];
+        return tiles[y][x];
     }
 
 }
